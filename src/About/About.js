d833f7fc3dad46befe1747251885dfcdd5060b6e
Devon Reihl <devon.reihl@gmail.com>
2020-10-06 18:57:15 -0700

    added image files and video urls

--- src/About/About.js
+++ src/About/About.js
@@ -7,7 +7,7 @@ export default class About extends React.Component {
       <section>
         <p>We are a supportive group of people coming together to spread random acts of kindness 
           as we participate in an international scavenger hunt For the Fun of It.</p><hr/>
-        <p>Learn more about <a href="https://www.gish.com/" target="_blank">GISH</a> before 
+        <p>Learn more about <a href="https://www.gish.com/" target="_blank" rel="noopener noreferrer">GISH</a> before 
           you join the fun?</p>
       </section>
 )
--- src/Gallery/Gallery.js
+++ src/Gallery/Gallery.js
@@ -1,51 +1,94 @@
 import React from 'react'
 import Iframe from 'react-iframe'
 import assbutt from './images/assbutt.jpg'
+// import workspaceForSpider from './images/workspaceForSpider.jpg'
+import teamPortrait from './images/teamPortrait.jpg'
+import toastedMarshmellowArt from './images/toastedMarshmellowArt.jpg'
+// import oregonNationalBird from './images/oregonNationalBird.jpg'
+// import whereNoManHasGoneBefore from './images/whereNoManHasGoneBefore.jpg'
+import monsterUnderTheBed from './images/monsterUnderTheBed.jpg'
+import JensenPie from './images/JensenPie.jpg'
+import fishingForComplements from './images/fishingForComplements.jpg'
+import ClootieDumpling from './images/ClootieDumpling.jpg'
+// import dogsInSpace from './images/dogsInSpac.jpg'
+import MishaCollins from './images/MishaCollins.jpg'
+import officePaperWeddingDress from './images/officePaperWeddingDress.jpg'
 import './Gallery.css'
 
 export default class Gallery extends React.Component {
   render() {
     return (
       <section>
-        <div class="gallery">
+        <div className="gallery">
+            <img src={MishaCollins} alt="GISH"/>
+        </div>
+        <div className="gallery">
+          <Iframe url="https://www.youtube.com/embed/jn5XrI-CuWE" allowFullScreen/>
+          <div className="desc">Add a description of the image here</div>
+        </div>
+        <div className="gallery">
+            <img src={officePaperWeddingDress} alt="GISH"/>
+          <div className="desc">Weddingdress made completely of office paper</div>
+        </div>
+        <div className="gallery">
+          <Iframe url="https://www.youtube.com/embed/CobyPnbQp08" allowFullScreen />
+          <div className="desc">Add a description of the image here</div>
+        </div>
+        <div className="gallery">
             <img src={assbutt} alt="GISH"/>
-          <div class="desc">Add a description of the image here</div>
+          <div className="desc">An Assbutt, part donkey-part butterfly</div>
         </div>
-        <div class="gallery">
-          <Iframe url="https://www.youtube.com/embed/tgbNymZ7vqY"/>
-          <div class="desc">Add a description of the image here</div>
+        <div className="gallery">
+          <Iframe url="https://www.youtube.com/embed/KoelVD7GP3I" allowfullscreen />
+          <div className="desc">Backwards poem (seriously play it backwards)</div>
         </div>
-        <div class="gallery">
-            <img src={assbutt} alt="GISH"/>
-          <div class="desc">Add a description of the image here</div>
+        <div className="gallery">
+            <img src={monsterUnderTheBed} alt="GISH"/>
+          <div className="desc">Sometimes the monsters get scared too</div>
         </div>
-        <div class="gallery">
-          <Iframe url="https://www.youtube.com/embed/tgbNymZ7vqY"/>
-          <div class="desc">Add a description of the image here</div>
+
+        <div className="gallery">
+          <Iframe url="https://www.youtube.com/embed/GCWwyJbiOJI" allowfullscreen />
+          <div className="desc">Add a description of the image here</div>
         </div>
-        <div class="gallery">
-            <img src={assbutt} alt="GISH"/>
-          <div class="desc">Add a description of the image here</div>
+        <div className="gallery">
+            <img src={JensenPie} alt="GISH"/>
+          <div className="desc">Pie crust portrait of Jensen Ackles</div>
         </div>
-        <div class="gallery">
-          <Iframe url="https://www.youtube.com/embed/tgbNymZ7vqY"/>
-          <div class="desc">Add a description of the image here</div>
+        <div classNameName="gallery">
+          <Iframe url="fill in" allowfullscreen/>
+          <div className="desc">Zipper egg</div>
         </div>
-        <div class="gallery">
-            <img src={assbutt} alt="GISH"/>
-          <div class="desc">Add a description of the image here</div>
+        <div className="gallery">
+            <img src={teamPortrait} alt="GISH"/>
+          <div className="desc">Team Literacy Bandits at there finest</div>
         </div>
-        <div class="gallery">
-          <Iframe url="https://www.youtube.com/embed/tgbNymZ7vqY"/>
-          <div class="desc">Add a description of the image here</div>
+        <div classNameName="gallery">
+          <Iframe url="https://www.youtube.com/embed/GCWwyJbiOJI" allowfullscreen/>
+          <div className="desc"></div>
         </div>
-        <div class="gallery">
-            <img src={assbutt} alt="GISH"/>
-          <div class="desc">Add a description of the image here</div>
+        <div className="gallery">
+            <img src={toastedMarshmellowArt} alt="GISH"/>
+          <div className="desc">Jared Padalecki burnt into a marshmellow</div>
         </div>
-        <div class="gallery">
-          <Iframe url="https://www.youtube.com/embed/tgbNymZ7vqY"/>
-          <div class="desc">Add a description of the image here</div>
+        <div classNameName="gallery">
+          <Iframe url="fill in" allowfullscreen/>
+          <div className="desc">Add a description of the image here</div>
+        </div>
+        <div className="gallery">
+            <img src={fishingForComplements} alt="GISH"/>
+          <div className="desc">Fishing for complements</div>
+        </div>
+        <div classNameName="gallery">
+          <Iframe url="fill in" allowfullscreen/>
+          <div className="desc">Add a description of the image here</div>
+        </div>
+        <div className="gallery">
+            <img src={ClootieDumpling} alt="GISH"/>
+        </div>
+        <div classNameName="gallery">
+          <Iframe url="https://www.youtube.com/embed/i_7cvm-gYyg" allowfullscreen/>
+          <div className="desc">Add a description of the image here</div>
         </div>
       </section>
     )
